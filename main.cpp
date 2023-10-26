e85e56293abbbb5ada48785d990fef113f94c73d
cs230426 <144984493+cs230426@users.noreply.github.com>
2023-10-26 11:47:20 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,61 @@
+//
+//  main.cpp
+//  WEEK7 LESSON2
+//
+//  Created by Shoxjaxon Kurbanov on 26/10/23.
+//
+
+//#include <iostream>
+//
+//int main(){
+//    int arr[3];
+//    arr[0] = 10;
+//    arr[1] = 20;
+//    arr[2] = 20;
+//    std::cout<<"the first element of the array is:"<<arr[0]<<std::endl;
+//
+//    return 0;
+//}
+
+//
+//#include <iostream>
+//
+//int main() {
+//    float numbers[4] = {1.2, 3.4, 5.6, 7.8};
+//    float lastElement = numbers[3];
+//
+//    std::cout << "the last element of the array is: " << lastElement << std::endl;
+//
+//    return 0;
+//}
+//
+//
+//#include <iostream>
+//
+//double calculatePower(double base, int exponent) {
+//    double result = 1.0;
+//    
+//    for(int i = 0; i = 0; i < exponent ; i++ ) {
+//        result *= base;
+//    }
+//    
+//    return result;
+//}
+//
+//int main() {
+//    double base;
+//    int exponent;
+//    
+//    std::cout << "Enter the base number: ";
+//    std::cin >> base;
+//    
+//    std::cout << "Entern the exponent:  ";
+//    std::cin >> exponent ;
+//    
+//    double power = calculatePower (base, exponent);
+//    
+//    std::cout << base << "raised to the power of " << exponent << "is: " << std::endl;
+//    
+//    
+//    return 0;
+//}
